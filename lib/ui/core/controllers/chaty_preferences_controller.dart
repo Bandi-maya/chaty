63b1aa4e28dc3b80b140448ed7105fdf1895f784
Bandi Maya <126802925+Bandi-maya@users.noreply.github.com>
2026-08-19 18:33:31 +0530

    feat: persist visual customization profiles

--- lib/ui/core/controllers/chaty_preferences_controller.dart
+++ lib/ui/core/controllers/chaty_preferences_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../../../domain/models/chaty_preferences.dart';
+import '../../../domain/models/visual_preferences.dart';
 import '../persistence/preferences_storage.dart';
 
 class PreferenceHistoryEntry {
@@ -26,9 +28,10 @@ class ChatyPreferencesController extends ChangeNotifier {
   NotificationPreferences _notification = const NotificationPreferences();
   MessageAutomationPreferences _automation = const MessageAutomationPreferences();
   NavigationEffectPreferences _effects = const NavigationEffectPreferences();
+  VisualPreferences _visual = const VisualPreferences();
 
-  final Set<String> _starredFavorites = {};
-  final List<PreferenceHistoryEntry> _history = [];
+  final Set<String> _starredFavorites = <String>{};
+  final List<PreferenceHistoryEntry> _history = <PreferenceHistoryEntry>[];
 
   ChatyPreferencesController() {
     _init();
@@ -41,29 +44,66 @@ class ChatyPreferencesController extends ChangeNotifier {
   NotificationPreferences get notification => _notification;
   MessageAutomationPreferences get automation => _automation;
   NavigationEffectPreferences get effects => _effects;
+  VisualPreferences get visual => _visual;
 
-  Set<String> get starredFavorites => Set.unmodifiable(_starredFavorites);
-  List<PreferenceHistoryEntry> get history => List.unmodifiable(_history);
+  Set<String> get starredFavorites => Set<String>.unmodifiable(_starredFavorites);
+  List<PreferenceHistoryEntry> get history =>
+      List<PreferenceHistoryEntry>.unmodifiable(_history);
 
   Future<void> _init() async {
     final data = await LocalPreferencesStorage.loadPreferences();
-    if (data.isNotEmpty) {
-      if (data['privacy'] != null) _privacy = PrivacyPreferences.fromMap(data['privacy']);
-      if (data['security'] != null) _security = SecurityPreferences.fromMap(data['security']);
-      if (data['home'] != null) _home = HomePreferences.fromMap(data['home']);
-      if (data['conversation'] != null) _conversation = ConversationPreferences.fromMap(data['conversation']);
-      if (data['notification'] != null) _notification = NotificationPreferences.fromMap(data['notification']);
-      if (data['automation'] != null) _automation = MessageAutomationPreferences.fromMap(data['automation']);
-      if (data['effects'] != null) _effects = NavigationEffectPreferences.fromMap(data['effects']);
-      if (data['favorites'] is List) {
-        _starredFavorites.addAll((data['favorites'] as List).map((e) => e.toString()));
-      }
-      notifyListeners();
+    if (data.isEmpty) return;
+
+    if (data['privacy'] is Map) {
+      _privacy = PrivacyPreferences.fromMap(
+        Map<String, dynamic>.from(data['privacy'] as Map),
+      );
     }
+    if (data['security'] is Map) {
+      _security = SecurityPreferences.fromMap(
+        Map<String, dynamic>.from(data['security'] as Map),
+      );
+    }
+    if (data['home'] is Map) {
+      _home = HomePreferences.fromMap(
+        Map<String, dynamic>.from(data['home'] as Map),
+      );
+    }
+    if (data['conversation'] is Map) {
+      _conversation = ConversationPreferences.fromMap(
+        Map<String, dynamic>.from(data['conversation'] as Map),
+      );
+    }
+    if (data['notification'] is Map) {
+      _notification = NotificationPreferences.fromMap(
+        Map<String, dynamic>.from(data['notification'] as Map),
+      );
+    }
+    if (data['automation'] is Map) {
+      _automation = MessageAutomationPreferences.fromMap(
+        Map<String, dynamic>.from(data['automation'] as Map),
+      );
+    }
+    if (data['effects'] is Map) {
+      _effects = NavigationEffectPreferences.fromMap(
+        Map<String, dynamic>.from(data['effects'] as Map),
+      );
+    }
+    if (data['visual'] is Map) {
+      _visual = VisualPreferences.fromMap(
+        Map<String, dynamic>.from(data['visual'] as Map),
+      );
+    }
+    if (data['favorites'] is List) {
+      _starredFavorites.addAll(
+        (data['favorites'] as List).map((e) => e.toString()),
+      );
+    }
+    notifyListeners();
   }
 
   void _persist() {
-    LocalPreferencesStorage.savePreferences({
+    LocalPreferencesStorage.savePreferences(<String, dynamic>{
       'privacy': _privacy.toMap(),
       'security': _security.toMap(),
       'home': _home.toMap(),
@@ -71,55 +111,109 @@ class ChatyPreferencesController extends ChangeNotifier {
       'notification': _notification.toMap(),
       'automation': _automation.toMap(),
       'effects': _effects.toMap(),
+      'visual': _visual.toMap(),
       'favorites': _starredFavorites.toList(),
     });
   }
 
-  void updatePrivacy(PrivacyPreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updatePrivacy(
+    PrivacyPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _privacy = newPrefs;
     if (logTitle != null) _logHistory('privacy', logTitle, prevVal, newVal);
     _persist();
     notifyListeners();
   }
 
-  void updateSecurity(SecurityPreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updateSecurity(
+    SecurityPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _security = newPrefs;
     if (logTitle != null) _logHistory('security', logTitle, prevVal, newVal);
     _persist();
     notifyListeners();
   }
 
-  void updateHome(HomePreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updateHome(
+    HomePreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _home = newPrefs;
     if (logTitle != null) _logHistory('home', logTitle, prevVal, newVal);
     _persist();
     notifyListeners();
   }
 
-  void updateConversation(ConversationPreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updateConversation(
+    ConversationPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _conversation = newPrefs;
-    if (logTitle != null) _logHistory('conversation', logTitle, prevVal, newVal);
+    if (logTitle != null) {
+      _logHistory('conversation', logTitle, prevVal, newVal);
+    }
     _persist();
     notifyListeners();
   }
 
-  void updateNotification(NotificationPreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updateNotification(
+    NotificationPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _notification = newPrefs;
-    if (logTitle != null) _logHistory('notification', logTitle, prevVal, newVal);
+    if (logTitle != null) {
+      _logHistory('notification', logTitle, prevVal, newVal);
+    }
     _persist();
     notifyListeners();
   }
 
-  void updateAutomation(MessageAutomationPreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updateAutomation(
+    MessageAutomationPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _automation = newPrefs;
-    if (logTitle != null) _logHistory('automation', logTitle, prevVal, newVal);
+    if (logTitle != null) {
+      _logHistory('automation', logTitle, prevVal, newVal);
+    }
     _persist();
     notifyListeners();
   }
 
-  void updateEffects(NavigationEffectPreferences newPrefs, {String? logTitle, dynamic prevVal, dynamic newVal}) {
+  void updateEffects(
+    NavigationEffectPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
     _effects = newPrefs;
     if (logTitle != null) _logHistory('effects', logTitle, prevVal, newVal);
+    _persist();
+    notifyListeners();
+  }
+
+  void updateVisual(
+    VisualPreferences newPrefs, {
+    String? logTitle,
+    dynamic prevVal,
+    dynamic newVal,
+  }) {
+    _visual = newPrefs;
+    if (logTitle != null) _logHistory('visual', logTitle, prevVal, newVal);
     _persist();
     notifyListeners();
   }
@@ -136,7 +230,12 @@ class ChatyPreferencesController extends ChangeNotifier {
 
   bool isFavorite(String settingKey) => _starredFavorites.contains(settingKey);
 
-  void _logHistory(String key, String title, dynamic prevVal, dynamic newVal) {
+  void _logHistory(
+    String key,
+    String title,
+    dynamic prevVal,
+    dynamic newVal,
+  ) {
     _history.insert(
       0,
       PreferenceHistoryEntry(
@@ -150,7 +249,6 @@ class ChatyPreferencesController extends ChangeNotifier {
     if (_history.length > 25) _history.removeLast();
   }
 
-  // Reset Categories
   void resetPrivacy() {
     _privacy = const PrivacyPreferences();
     _persist();
@@ -181,6 +279,12 @@ class ChatyPreferencesController extends ChangeNotifier {
     notifyListeners();
   }
 
+  void resetVisual() {
+    _visual = const VisualPreferences();
+    _persist();
+    notifyListeners();
+  }
+
   void resetAll() {
     _privacy = const PrivacyPreferences();
     _security = const SecurityPreferences();
@@ -189,6 +293,7 @@ class ChatyPreferencesController extends ChangeNotifier {
     _notification = const NotificationPreferences();
     _automation = const MessageAutomationPreferences();
     _effects = const NavigationEffectPreferences();
+    _visual = const VisualPreferences();
     _starredFavorites.clear();
     _history.clear();
     _persist();
